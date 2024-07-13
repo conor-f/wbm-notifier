59e0088ffa7a01411409e2d5c6fc7bb9cd952339
Conor Flynn <conor.j.flynn@gmail.com>
2024-07-13 13:31:52 +0200

    Removes debug statements (0.2.1)

--- src/wbm_notifier/storage.py
+++ src/wbm_notifier/storage.py
@@ -2,14 +2,10 @@ import shelve
 
 
 def get_previous_content(shelf_path, key):
-    print(f"Gotten key (get): {key}")
     with shelve.open(shelf_path) as shelf:
-        print(shelf.get(key, False))
         return shelf.get(key, False)
 
 
 def store_content(shelf_path, key, content):
-    print(f"Gotten key (store): {key}")
-    print(content)
     with shelve.open(shelf_path) as shelf:
         shelf[key] = content
