12ac69ff31335f6558bf1da1779b753efc45bc2d
Conor Flynn <conor.j.flynn@gmail.com>
2024-07-16 15:54:17 +0200

    Supports pushing to Telegram using Apprise (0.5.0)

--- src/config/manager.py
+++ src/config/manager.py
@@ -26,12 +26,41 @@ class Config:
             type=int,
             help="Interval in minutes to notify if no new listings are found",
         )
+        parser.add_argument(
+            "--telegram-bot-auth", type=str, help="Telegram bot authentication token"
+        )
+        parser.add_argument("--telegram-chat-id", type=str, help="Telegram chat ID")
         args = parser.parse_args()
 
         self.config = configparser.ConfigParser()
         self.config.read(config_file)
 
+        self.config = configparser.ConfigParser()
+        self.config.read(config_file)
+
         self.URL = args.url or os.getenv("URL", self.config["DEFAULT"]["URL"])
+        self.SHELF_PATH = args.shelf_path or os.getenv(
+            "SHELF_PATH", self.config["DEFAULT"]["SHELF_PATH"]
+        )
+        self.TOPIC = args.topic or os.getenv("TOPIC", self.config["DEFAULT"]["TOPIC"])
+        self.NTFY_SERVER = args.ntfy_server or os.getenv(
+            "NTFY_SERVER", self.config["DEFAULT"]["NTFY_SERVER"]
+        )
+        self.CHECK_INTERVAL = args.check_interval or int(
+            os.getenv("CHECK_INTERVAL", self.config["DEFAULT"]["CHECK_INTERVAL"])
+        )
+        self.NO_NOTIFICATION_INTERVAL = args.no_notification_interval or int(
+            os.getenv(
+                "NO_NOTIFICATION_INTERVAL",
+                self.config["DEFAULT"]["NO_NOTIFICATION_INTERVAL"],
+            )
+        )
+        self.TELEGRAM_BOT_AUTH = args.telegram_bot_auth or os.getenv(
+            "TELEGRAM_BOT_AUTH", self.config["DEFAULT"].get("TELEGRAM_BOT_AUTH", None)
+        )
+        self.TELEGRAM_CHAT_ID = args.telegram_chat_id or os.getenv(
+            "TELEGRAM_CHAT_ID", self.config["DEFAULT"].get("TELEGRAM_CHAT_ID", None)
+        )
         self.SHELF_PATH = args.shelf_path or os.getenv(
             "SHELF_PATH", self.config["DEFAULT"]["SHELF_PATH"]
         )
